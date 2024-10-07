e8301b4b3a025ae8a531266e6babc88a47f9d1d3
kanishqdhangar <dhangarkanishq@gmail.com>
2024-10-07 09:48:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
